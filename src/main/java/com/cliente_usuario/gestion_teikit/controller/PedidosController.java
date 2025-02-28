4b84c2df317cecd25f06c64c2832ab3fa69075b2
Teikit <teikit.app@gmail.com>
2025-02-28 00:08:56 -0300

    Update PedidosController.java

--- src/main/java/com/cliente_usuario/gestion_teikit/controller/PedidosController.java
+++ src/main/java/com/cliente_usuario/gestion_teikit/controller/PedidosController.java
@@ -10,10 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @CrossOrigin(origins = "${frontend.api.url}")
 @RestController
